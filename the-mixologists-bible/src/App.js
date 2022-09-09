aa457f76775e14ec4314550b01aa3e234ae9fa4a
Alexandro Valdez <alexandroivaldez@gmail.com>
2022-09-09 18:03:12 -0500

    Update App.js

--- the-mixologists-bible/src/App.js
+++ the-mixologists-bible/src/App.js
@@ -1,26 +1,31 @@
+import { click } from '@testing-library/user-event/dist/click';
 import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
       recipes: []
     }
   }
 
   componentDidMount() {
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin')
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
       .then(response => response.json())
       .then(recipe => this.setState({ recipes: recipe }));
-      console.log(this.state.recipes);
   }
+
+  clickHandler = () => {
+    console.log();
+    alert(this.state.recipes.drinks[0].strDrink)
+  }
+
 
   render() {
     return (
       <div className="App">
-        {console.log(this.state.recipes)}
+        <button onClick={this.clickHandler}>Test</button>
       </div>
     );
   }
